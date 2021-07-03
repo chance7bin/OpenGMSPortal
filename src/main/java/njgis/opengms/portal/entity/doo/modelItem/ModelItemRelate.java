ace977a43058b9b236320b1eb7ee0be7273e2cbf
Sirius <921485453@qq.com>
2021-07-03 16:31:53 +0800

    添加ModelItem及相关类

---
+++ src/main/java/njgis/opengms/portal/entity/doo/modelItem/ModelItemRelate.java
@@ -0,0 +1,44 @@
+package njgis.opengms.portal.entity.doo.modelItem;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName ModelItemRelate
+ * @Description todo
+ * @Author Kai
+ * @Date 2019/2/21
+ * @Version 1.0.0
+ * TODO ModelItem 关联信息
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelItemRelate {
+
+    List<String> data = new ArrayList<>();
+
+    List<ModelRelation> modelRelationList = new ArrayList<>();
+
+    List<String> modelItems = new ArrayList<>();
+    List<String> conceptualModels = new ArrayList<>();
+    List<String> logicalModels = new ArrayList<>();
+    List<String> computableModels = new ArrayList<>();
+    List<String> dataItems = new ArrayList<>();
+
+    List<String> concepts = new ArrayList<>();
+    List<String> spatialReferences = new ArrayList<>();
+    List<String> templates = new ArrayList<>();
+    List<String> units = new ArrayList<>();
+
+    List<Map<String,String>> localFiles = new ArrayList<Map<String,String>>();
+    List<Map<String,String>> dataSpaceFiles = new ArrayList<Map<String,String>>();
+    List<Map<String,String>> exLinks = new ArrayList<Map<String,String>>();
+
+}
