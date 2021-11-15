fd6e78d0bd1596eb889c664867ca1b09f93d1814
7bin <782807969@qq.com>
2021-11-15 14:24:31 +0800

    2021/11/15 update

--- src/main/java/njgis/opengms/portal/controller/TaskController.java
+++ src/main/java/njgis/opengms/portal/controller/TaskController.java
@@ -2,6 +2,7 @@ package njgis.opengms.portal.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.ApiOperation;
+import njgis.opengms.portal.component.LoginRequired;
 import njgis.opengms.portal.entity.doo.JsonResult;
 import njgis.opengms.portal.entity.dto.task.TaskCheckDTO;
 import njgis.opengms.portal.entity.dto.task.TaskFindDTO;
@@ -24,7 +25,7 @@ public class TaskController {
     @Autowired
     TaskService taskService;
 
-    // @LoginRequired
+    @LoginRequired
     @ApiOperation(value = "Task初始化API，获取模型描述信息，State信息，task以及Dx相关信息 [ /TaskInit/{id} ]")
     @RequestMapping(value = "/init/{id}", method = RequestMethod.GET)
     public JsonResult initTask(@PathVariable("id") String id, HttpServletRequest request){
@@ -35,7 +36,7 @@ public class TaskController {
     }
 
 
-    // @LoginRequired
+    @LoginRequired
     @ApiOperation(value = "加载默认测试数据，返回数据成功上传之后的url")
     @RequestMapping(value = "/loadTestData/{modelId}", method = RequestMethod.POST)
     public JsonResult loadTestData(@PathVariable String modelId, HttpServletRequest request){
@@ -48,7 +49,7 @@ public class TaskController {
 
     }
 
-    // @LoginRequired
+    @LoginRequired
     @ApiOperation(value = "调用模型 lists中的oid就是computableModel的id")
     @RequestMapping(value = "/invoke", method = RequestMethod.POST)
     public JsonResult invoke(@RequestBody JSONObject lists, HttpServletRequest request) {
@@ -70,7 +71,7 @@ public class TaskController {
     }
 
 
-    // @LoginRequired
+    @LoginRequired
     @ApiOperation(value = "得到任务信息 [ /getTasksByUserIdByStatus ]")
     @RequestMapping(value="/taskInfo",method = RequestMethod.POST )
     public JsonResult getTasksByUserByStatus(@RequestBody TaskFindDTO taskFindDTO, HttpServletRequest request) {
@@ -93,5 +94,8 @@ public class TaskController {
     //     return taskService.updateUserTasks(email,taskFindDTO);
     //
     // }
+    
+
+
 
 }
