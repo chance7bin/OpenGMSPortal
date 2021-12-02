8fc035365a6e3218e1f199caae535d285ee4808b
wangyijie <371252847@qq.com>
2021-12-02 10:58:49 +0800

    更新

--- src/main/resources/static/js/managementSystem/manage_home.js
+++ src/main/resources/static/js/managementSystem/manage_home.js
@@ -1,28 +1,188 @@
 new Vue({
     el: '#manage_home',
     data:{
-            todos:[{
-                name:"abc",
-                age:"12"
-            },{
-                name:"def",
-                age:"13"
-            }],
+        serverNodes:[], //服务节点
+        serverNodeCoods:[],//服务节点坐标
+
+        resourceCount:[], //资源数量
+
+        pageViewCount:[],//页面访问数量
+        userViewCount:[], //用户访问数量
+
     },
     mounted() {
+        this.getServerNodes()
 
-        axios.get('/management/test')
-            //这里使用用箭头函数，使得this指向不是windows，而是外层的vue
-            .then( (response)=> {
-                console.log(response);
-                this.todos.push(response.data.data)
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        this.getPageViewCount()
 
+        this.getUserViewCount()
     },
     methods: {
+        //获取服务节点数据
+        getServerNodes(){
+            axios.get("/managementSystem/serverNodes")
+                .then(response=> {
+                    this.serverNodes=response.data.data.data
+                    for(let i=0,len=this.serverNodes.length;i<len;i++){
+                        this.serverNodeCoods.push([parseFloat(this.serverNodes[i].geoJson.longitude),parseFloat(this.serverNodes[i].geoJson.latitude)])
+                    }
+                    this.loadEarth()
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+
+        //绘制地球
+        loadEarth(){
+            let myChart = echarts.init(document.getElementById('earthNodes'));
+            let ROOT_PATH ='https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples';
+
+            myChart.setOption({
+                backgroundColor: '#081f57',
+                globe: {
+                    baseTexture: ROOT_PATH + '/data-gl/asset/world.topo.bathy.200401.jpg',
+                    heightTexture:
+                        ROOT_PATH + '/data-gl/asset/bathymetry_bw_composite_4k.jpg',
+                    shading: 'lambert',
+                    light: {
+                        ambient: {
+                            intensity: 1
+                        },
+                        main: {
+                            intensity: 1
+                        }
+                    },
+                    viewControl: {
+                        autoRotate: true, // 是否开启视角绕物体的自动旋转查看
+                        autoRotateSpeed: 20, //物体自转的速度,单位为角度 / 秒，默认为10 ，也就是36秒转一圈。
+                        autoRotateAfterStill: 2, // 在鼠标静止操作后恢复自动旋转的时间间隔,默认 3s
+                        rotateSensitivity: 3, // 旋转操作的灵敏度，值越大越灵敏.设置为0后无法旋转。[1, 0]只能横向旋转.[0, 1]只能纵向旋转
+                        targetCoord: [118.7778, 32.0617], // 定位到北京
+                        // maxDistance: 100,
+                        // minDistance: 300
+                    }
+                },
+                series: {
+                    type: 'scatter3D',
+                    coordinateSystem: 'globe',
+                    blendMode: 'lighter',
+                    symbolSize: 8, // 点位大小
+                    itemStyle: {
+                        color: '#f00' ,// 各个点位的颜色设置
+                        opacity: 1, // 透明度
+                        borderWidth: 1, // 边框宽度
+                        borderColor: 'rgba(20,15,2,0.8)' //rgba(180, 31, 107, 0.8)
+                    },
+                    data: this.serverNodeCoods
+                }
+            });
+        },
+
+        //获取资源数量
+        getResourceCount(){
+            let itemTypes=["DataItem", "ModelItem", "ConceptualModel", "LogicalModel", "ComputableModel", "Concept", "SpatialReference", "Template", "Unit", "Theme", "DataHub", "DataMethod", "Article", "Project", "Conference", "Version", "Information"]
+            itemTypes.map((itemType)=>{
+                axios.get("/managementSystem/item/count/"+itemType)
+                    .then(response=> {
+                        this.resourceCount.push({itemType:response.data.data})
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            })
+        },
+
+        //获取页面访问数量
+        getPageViewCount(){
+            axios.get("/managementSystem/view/page/count")
+                .then(response=> {
+                    this.pageViewCount=response.data.data
+                    this.drawPageViewCount()
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+
+        //绘制页面访问图
+        drawPageViewCount(){
+            let myChart =echarts.init( document.getElementById('pageView'));
+            let xData=this.pageViewCount.map((item=>{
+                return item.date
+            }))
+            let yData=this.pageViewCount.map((item=>{
+                return item.count
+            }))
+            console.log(xData,yData)
+
+            myChart.setOption({
+                title: {
+                    text: '页面访问数量'
+                },
+                xAxis: {
+                    type: 'category',
+                    data: xData
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        data: yData,
+                        type: 'line',
+                        smooth: true
+                    }
+                ]
+            })
+
+        },
+
+        //获取用户访问数量
+        getUserViewCount(){
+            axios.get("/managementSystem/view/user/count")
+                .then(response=> {
+                    console.log(response)
+                    this.userViewCount=response.data.data
+                    this.drawUserViewCount()
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+
+        //绘制用户访问数量
+        drawUserViewCount(){
+            let myChart =echarts.init( document.getElementById('userView'));
+            let xData=this.userViewCount.map((item=>{
+                return item.date
+            }))
+            let yData=this.userViewCount.map((item=>{
+                return item.count
+            }))
+            console.log(xData,yData)
+
+            myChart.setOption({
+                title: {
+                    text: '用户访问数量'
+                },
+                xAxis: {
+                    type: 'category',
+                    data: xData
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        data: yData,
+                        type: 'line',
+                        smooth: true
+                    }
+                ]
+            })
+        },
+
 
     },
 })
--- src/main/resources/static/js/managementSystem/manage_system.js
+++ src/main/resources/static/js/managementSystem/manage_system.js
@@ -65,9 +65,27 @@ new Vue({
         //未处理版本审核
         searchInputWaitVersion:"", //版本搜索
         waitVersionTableData:[],// 表格数据
-        currentPageWaitVersion:"", //当前页
-        PageSizeWaitVersion:"", //当前页大小
-        totalWaitVersion:"", //总数
+        currentPageWaitVersion:1, //当前页
+        PageSizeWaitVersion:10, //当前页大小
+        totalWaitVersion:0, //总数
+        dialogVersionComp:false, //版本对比对话框显示控制
+        versionCompData:{}, //版本对比数据
+        versionCompChangedField:{}, //版本对比数据中修改项
+
+
+        //接受版本审核
+        searchInputAcceptVersion:"", //版本搜索
+        acceptVersionTableData:[],// 表格数据
+        currentPageAcceptVersion:1, //当前页
+        PageSizeAcceptVersion:10, //当前页大小
+        totalAcceptVersion:0, //总数
+
+        //接受版本审核
+        searchInputRejectVersion:"", //版本搜索
+        rejectVersionTableData:[],// 表格数据
+        currentPageRejectVersion:1, //当前页
+        PageSizeRejectVersion:10, //当前页大小
+        totalRejectVersion:0, //总数
 
     },
 
@@ -87,7 +105,7 @@ new Vue({
             }else if(pageId===3){
                 this.getGeoItemList();
             }else{
-
+                this.getWaitVersionList();
             }
         },
 
@@ -341,7 +359,6 @@ new Vue({
                 "sortField": "createTime"
             })
                 .then(response=> {
-                    console.log(response)
                     this.userTableData=response.data.data.content
                     this.totalUser=response.data.data.totalElements
                 })
@@ -396,10 +413,8 @@ new Vue({
                 "curQueryField": "name",
             })
                 .then(response=> {
-                    this.geoItemTableData=response.data.data.allPortalItem
+                    this.geoItemTableData=response.data.data.itemList
                     this.totalGeo=response.data.data.totalElements
-                    console.log(response)
-
                 })
                 .catch(function (error) {
                     console.log(error);
@@ -424,12 +439,50 @@ new Vue({
 
 
         //版本审核管理界面================================================
+
+        changeTabs(tab){
+            if(tab.index==="0"){
+                this.getWaitVersionList()
+            }else if(tab.index==="1"){
+                this.getAcceptVersionList()
+            }else{
+                this.getRejectVersionList()
+            }
+        },
+
         //未处理部分
         //查询未处理版本
         getWaitVersionList(){
-
+            axios.post('/version/versionList/uncheck', {
+                "asc": false,
+                "page": this.currentPageWaitVersion,
+                "pageSize": this.PageSizeWaitVersion,
+                "searchText": this.searchInputWaitVersion,
+                "sortField": "createTime",
+            })
+                .then(response=> {
+                    console.log(response)
+                    this.waitVersionTableData=response.data.data.content
+                    this.totalWaitVersion=response.data.data.totalElements
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         },
-        viewVersion(){
+
+        //查看某次审核，对比
+        viewWaitVersion(versionId){
+            this.dialogVersionComp=true
+            axios.get("/version/detail/"+versionId)
+                .then(response=> {
+                    this.versionCompData=response.data.data
+                    this.versionCompChangedField=this.versionCompData.changedField
+                    console.log(this.versionCompChangedField);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            console.log(versionId)
 
         },
         acceptVersion(){
@@ -443,6 +496,68 @@ new Vue({
 
         },
         handleCurrentChangeWaitVersion(){
+
+        },
+
+        //接受部分
+        //查询接受版本
+        getAcceptVersionList(){
+            axios.post('/version/versionList/accepted', {
+                "asc": false,
+                "page": this.currentPageAcceptVersion,
+                "pageSize": this.PageSizeAcceptVersion,
+                "searchText": this.searchInputAcceptVersion,
+                "sortField": "createTime",
+            })
+                .then(response=> {
+                    this.acceptVersionTableData=response.data.data.content
+                    this.totalAcceptVersion=response.data.data.totalElements
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        viewAcceptVersion(){
+
+        },
+
+        handleSizeChangeAcceptVersion(){
+
+        },
+        handleCurrentChangeAcceptVersion(){
+
+        },
+
+        //拒绝部分
+        //查询拒绝版本
+        getRejectVersionList(){
+            axios.post('/version/versionList/rejected', {
+                "asc": false,
+                "page": this.currentPageRejectVersion,
+                "pageSize": this.PageSizeRejectVersion,
+                "searchText": this.searchInputRejectVersion,
+                "sortField": "createTime",
+            })
+                .then(response=> {
+                    console.log(response)
+                    this.rejectVersionTableData=response.data.data.content
+                    this.totalRejectVersion=response.data.data.totalElements
+                    console.log(this.rejectVersionTableData)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+
+
+        viewRejectVersion(){
+
+        },
+
+        handleSizeChangeRejectVersion(){
+
+        },
+        handleCurrentChangeRejectVersion(){
 
         },
 
