fc87be0af702fcfd41a80ab1e2ff68d348d04ea6
7bin <782807969@qq.com>
2022-05-26 15:01:15 +0800

    2022/5/26 management system comment module

--- src/main/java/njgis/opengms/portal/entity/po/Comment.java
+++ src/main/java/njgis/opengms/portal/entity/po/Comment.java
@@ -1,5 +1,6 @@
 package njgis.opengms.portal.entity.po;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import njgis.opengms.portal.entity.doo.base.PortalId;
 import njgis.opengms.portal.enums.ItemTypeEnum;
@@ -24,6 +25,8 @@ public class Comment extends PortalId {
 
     int thumbsUpNumber = 0;
     int readStatus;//标记是否已读
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     Date createTime;
 
     Boolean isPublic = false; // 该评论是否对公众开放，如不开放，则只有评论者，被评论人，条目创建者、管理者，以及门户管理员才能看到。
